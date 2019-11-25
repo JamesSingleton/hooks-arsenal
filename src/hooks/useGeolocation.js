320ef601bf65bbefbbb9d2d050b252e3b0171584
Michael Mahrous Tobia <michael.m.tobia@aexp.com>
2019-11-24 19:24:41 -0500

    feat(useGeolocation): hook with api

---
+++ src/hooks/useGeolocation.js
@@ -0,0 +1,69 @@
+import React from 'react';
+
+const defaultPositionOptions = {
+  enableHighAccuracy: true,
+  timeout: 5000,
+  maximumAge: 0,
+};
+
+export default function useGeolocation(initialPosition = null) {
+  const [_position, setPosition] = React.useState(initialPosition);
+  const [_watchID, setWatchID] = React.useState(null);
+
+  function clearWatch() {
+    if (_watchID) {
+      navigator.geolocation.clearWatch(_watchID);
+      setWatchID(null);
+    }
+  }
+
+  React.useEffect(() => () => clearWatch(), []);
+
+  return {
+    clearWatch,
+    get position() {
+      return _position;
+    },
+    get watching() {
+      return _watchID !== null;
+    },
+    get watchId() {
+      return _watchID;
+    },
+    getCurrentPosition(options = defaultPositionOptions) {
+      return new Promise((resolve, reject) => {
+        navigator.geolocation.getCurrentPosition(
+          (position) => {
+            setPosition(position);
+
+            resolve(position);
+          },
+          reject,
+          options
+        );
+      });
+    },
+    watchPosition(options = defaultPositionOptions) {
+      if (_watchID) {
+        return Promise.resolve(_position);
+      }
+
+      return new Promise((resolve, reject) => {
+        const id = navigator.geolocation.watchPosition(
+          (position) => {
+            setPosition(position);
+
+            if (_watchID === null) {
+              setWatchID(id);
+              resolve(position);
+            }
+          },
+          reject,
+          options
+        );
+      });
+    },
+  };
+}
+
+useGeolocation.defaults = {};
--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 /* eslint-disable import/prefer-default-export */
 export { default as useLocalStorage } from './hooks/useLocalStorage';
 export { default as useSessionStorage } from './hooks/useSessionStorage';
+export { default as useGeolocation } from './hooks/useGeolocation';
