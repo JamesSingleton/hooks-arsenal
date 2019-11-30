d7510facdc1366b71601a8e6fe5c5f9a123a1f93
Paul Richard <heythisispaul@gmail.com>
2019-11-29 19:47:04 -0700

    feat(hooks): Add useHandleForm

---
+++ __tests__/hooks/useHandleForm.js
@@ -0,0 +1,83 @@
+import { renderHook, act } from '@testing-library/react-hooks';
+import useFormSubmissionHandling from '../../src/hooks/useHandleForm';
+
+// eslint-disable-next-line max-params
+const simulateEvent = (name, value, type, checked) => ({
+  preventDefault: jest.fn(),
+  target: {
+    value, type, name, checked,
+  },
+});
+
+describe('useFormSubmissionHandling', () => {
+  const submitFunctionMock = jest.fn();
+  const mockEvent = simulateEvent();
+  const mockFormValue = { greeting: 'hello!' };
+  const prehookValue = 'preSubmithook Value!';
+
+  beforeEach(() => submitFunctionMock.mockReset());
+
+  it('should update formValue on event changes', () => {
+    const { result } = renderHook(() => useFormSubmissionHandling());
+    const { handleUpdate } = result.current;
+    act(() => handleUpdate(simulateEvent('greeting', 'hello!')));
+    expect(result.current.formValues).toEqual(mockFormValue);
+  });
+
+  it('can update formValue from checkbox changes', () => {
+    const { result } = renderHook(() => useFormSubmissionHandling());
+    const { handleUpdate } = result.current;
+    act(() => handleUpdate(simulateEvent('checkboxTime', null, 'checkbox', true)));
+    expect(result.current.formValues).toEqual({ checkboxTime: true });
+  });
+
+  it('calls the provided function when the form is submitted with the form values', async () => {
+    expect.assertions(2);
+    const { result, waitForNextUpdate } = renderHook(() => useFormSubmissionHandling({
+      submitFunction: submitFunctionMock,
+      initialValue: mockFormValue,
+    }));
+    const { handleSubmit } = result.current;
+    act(() => {
+      handleSubmit(mockEvent);
+    });
+    await waitForNextUpdate();
+    expect(submitFunctionMock.mock.calls[0][0]).toEqual(mockFormValue);
+    expect(submitFunctionMock).toHaveReturned();
+  });
+
+  it('updates the state on submit correctly', async () => {
+    expect.assertions(4);
+    const { result, waitForNextUpdate } = renderHook(() => useFormSubmissionHandling({
+      submitFunction: submitFunctionMock,
+      initialValue: mockFormValue,
+    }));
+    const { handleSubmit } = result.current;
+    act(() => {
+      handleSubmit(mockEvent);
+    });
+    const { isSubmitting, experiencedError } = result.current;
+    expect(isSubmitting).toEqual(true);
+    expect(experiencedError).toEqual(false);
+    expect(result.current.submitResult).toEqual(null);
+    await waitForNextUpdate();
+    expect(result.current.isSubmitting).toEqual(false);
+  });
+
+  it('updates the error state correctly', async () => {
+    expect.assertions(2);
+    const errorMock = jest.fn(() => Promise.reject(new Error('error time')));
+    const preSubmitHook = jest.fn(() => prehookValue);
+    const { result, waitForNextUpdate } = renderHook(() => useFormSubmissionHandling({
+      submitFunction: errorMock,
+      preSubmitHook,
+    }));
+    act(() => {
+      result.current.handleSubmit(mockEvent);
+    });
+    await waitForNextUpdate();
+    const { isSubmitting, experiencedError } = result.current;
+    expect(isSubmitting).toBe(false);
+    expect(experiencedError).toBe(true);
+  });
+});
---
+++ src/hooks/useHandleForm.js
@@ -0,0 +1,47 @@
+import { useState, useCallback } from 'react';
+
+const useFormSubmission = ({ submitFunction, initialValue = {} } = {}) => {
+  const [formValues, setFormValues] = useState(initialValue);
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const [experiencedError, setExperiencedError] = useState(false);
+  const [submitResult, setSubmitResult] = useState(null);
+
+  const handleUpdate = useCallback((event) => {
+    const {
+      name, value, checked, type,
+    } = event.target;
+    const isCheckbox = (type === 'checkbox');
+    const newFormData = { ...formValues, [name]: isCheckbox ? checked : value };
+    setFormValues(newFormData);
+  }, [formValues]);
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      setExperiencedError(false);
+      setIsSubmitting(true);
+      setSubmitResult(null);
+      const result = await submitFunction(formValues);
+      setIsSubmitting(false);
+      setSubmitResult(result);
+      return result;
+    } catch (err) {
+      // eslint-disable-next-line no-console
+      console.error(err);
+      setExperiencedError(true);
+      setIsSubmitting(false);
+      return err;
+    }
+  };
+
+  return {
+    handleUpdate,
+    formValues,
+    handleSubmit,
+    isSubmitting,
+    experiencedError,
+    submitResult,
+  };
+};
+
+export default useFormSubmission;
