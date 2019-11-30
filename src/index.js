9a17852f9109b45fc986c45dabd6801222ca1b2a
Paul Richard <heythisispaul@gmail.com>
2019-11-29 19:47:04 -0700

    feat(hooks): Add useHandleForm

--- src/index.js
+++ src/index.js
@@ -3,3 +3,4 @@ export { default as useLocalStorage } from './hooks/useLocalStorage';
 export { default as useSessionStorage } from './hooks/useSessionStorage';
 export { default as useGeolocation } from './hooks/useGeolocation';
 export { default as useWindowDimensions } from './hooks/useWindowDimensions';
+export { default as useHandleForm } from './hooks/useHandleForm';
