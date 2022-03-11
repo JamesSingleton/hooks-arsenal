5e33fae9b26612494244bb95a5004de85a26d9c6
James Singleton <jamesrsingleton1@gmail.com>
2022-03-11 12:32:23 -0700

    chore: update docs link

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -37,7 +37,7 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       navbar: {
-        title: 'My Site',
+        title: 'Hooks Arsenal',
         logo: {
           alt: 'My Site Logo',
           src: 'img/logo.svg',
@@ -47,7 +47,7 @@ const config = {
             type: 'doc',
             docId: 'overview',
             position: 'left',
-            label: 'Tutorial',
+            label: 'Docs',
           },
           {
             href: 'https://github.com/facebook/docusaurus',
